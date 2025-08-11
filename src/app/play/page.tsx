4267490926a3829f30cad3fc9188983a3de374b8
willowv <eps.willow@gmail.com>
2025-08-11 14:19:52 -0700

    Initial folder/file structure

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,14 +1,15 @@
-import Link from "next/link";
+'use client'
 
 export default function Home() {
   return (
     <div className="flex gap-4 items-center flex-col">
-      <Link
+      <p>Current Score: 5</p>
+      <button
         className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-        href="./play"
+        onClick={()=>{}}
       >
-        Play
-      </Link>
+        End Turn
+      </button>
     </div>
   );
 }
