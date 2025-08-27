677e053a26019bdc49d97d21612f380822586030
willowv <eps.willow@gmail.com>
2025-08-27 11:56:04 -0700

    Additional sql queries

--- src/app/scores/Scores.tsx
+++ src/app/scores/Scores.tsx
@@ -1,18 +1,57 @@
 import { getTodayWithoutTime } from 'app/util'
-import { getScoresByDate } from './data'
+import * as Data from './data'
 
 export default async function Scores() {
+    // Different display based on number of scores for this day
+    // >10 scores: histogram
+    // <10 scores: current top 10
+    // No scores: 'there are no scores yet! be the first'
+    // Get count of scores for this date
     const date = getTodayWithoutTime()
-    const scores = await getScoresByDate(date)
+    const [count, min, max] = await Data.getScoreStatsByDate(date)
+
+    if (count == 0)
+        return (
+            <div className="text-foreground text-md m-2 rounded-lg p-2 text-center backdrop-blur-xl">
+                {'There are no scores recorded yet for today!'}
+            </div>
+        )
+
+    // Don't bother with histogram for small count or overly tight buckets
+    if (count < 10 || max - min < 5) {
+        // What are the current top 5 scores?
+        const scores = await Data.getTopNScoresByDate(date, 5)
+        return (
+            <div className="flex flex-col gap-2">
+                {scores.map((score, index) => {
+                    return (
+                        <div
+                            key={index}
+                            className="text-foreground text-md m-2 rounded-lg p-2 text-center backdrop-blur-xl"
+                        >
+                            {`${score.score} in ${score.numturns} turns`}
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+
+    const buckets = await Data.getScoreBucketsByDate(date, min, max, 5)
+    const bucketIncrement = (max - min) / 5
     return (
         <div className="flex flex-col gap-2">
-            {scores.map((score, index) => {
+            {buckets.map((bucket, index) => {
+                const bucketMin = Math.floor(min + bucketIncrement * index)
+                const bucketMax = Math.floor(
+                    min + bucketIncrement * (index + 1) - 1
+                )
                 return (
                     <div
                         key={index}
                         className="text-foreground text-md m-2 rounded-lg p-2 text-center backdrop-blur-xl"
                     >
-                        {`${score.username} - ${score.score} in ${score.numTurns} turns`}
+                        {`${bucketMin}-${bucketMax} | ${bucket.frequency} players`}
                     </div>
                 )
             })}
--- src/app/scores/data.ts
+++ src/app/scores/data.ts
@@ -4,19 +4,64 @@ interface Score {
     readonly id: number
     readonly username: string
     readonly score: number
-    readonly numTurns: number
+    readonly numturns: number
     readonly date: Date
+}
+
+interface ScoreStats {
+    readonly id: number
+    readonly count: number
+    readonly min: number
+    readonly max: number
+}
+
+interface Bucket {
+    readonly bucketNumber: number
+    readonly frequency: number
 }
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
 
-export async function getScoresByDate(date: Date) {
-    // Simulate slow fetch for development
-    await new Promise((resolve) => setTimeout(resolve, 3000))
+export async function getTopNScoresByDate(date: Date, n: number) {
     return await sql<Score[]>`
   SELECT *
   FROM scores
-  WHERE scores.date = ${date.toISOString()}
-  ORDER BY scores.score DESC
-  LIMIT 10`
+  WHERE date = ${date.toISOString()}
+  ORDER BY score DESC, numturns ASC
+  LIMIT ${n}`
+}
+
+export async function getScoreStatsByDate(date: Date) {
+    const [row] = await sql<ScoreStats[]>`
+    WITH today_scores AS (
+    SELECT score
+    FROM scores
+    WHERE date = ${date.toISOString()}
+)
+  SELECT COUNT(score), MIN(score), MAX(score)
+  FROM today_scores`
+    return [row.count, row.min, row.max]
+}
+
+export async function getScoreBucketsByDate(
+    date: Date,
+    min: number,
+    max: number,
+    numBuckets: number
+) {
+    return await sql<Bucket[]>`
+  WITH buckets_cte AS (
+    SELECT WIDTH_BUCKET(score, ${min}, ${max}, ${numBuckets}) AS bucket_number
+    FROM scores
+    WHERE date = ${date.toISOString()}
+)
+SELECT
+    bucket_number,
+    COUNT(*) AS frequency
+FROM
+    buckets_cte
+GROUP BY
+    bucket_number
+ORDER BY
+    bucket_number;`
 }
