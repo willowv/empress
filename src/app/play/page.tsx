93c48ee07c6a150944774d98b0b454d1d66e0528
willowv <eps.willow@gmail.com>
2025-08-28 12:09:59 -0700

    prepare for date selection

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -2,12 +2,37 @@
 
 import { useState } from 'react'
 import Game from '@/game/Game'
-import { getTodayWithoutTime } from 'app/util'
+import { dateOnlyString, getTodayWithoutTime } from 'app/util'
+import Chariot from '@/svg/tarot/Chariot'
+import Button from '@/ui/Button'
 export default function Home() {
     const [selectedDate] = useState<Date>(getTodayWithoutTime)
+    const [isPlaying, setIsPlaying] = useState<boolean>(false)
+    if (isPlaying)
+        return (
+            <div className="not-motion-reduce:animate-slidefrombottom">
+                <Game date={selectedDate} />
+            </div>
+        )
+
     return (
-        <div className="not-motion-reduce:animate-slidefromright">
-            <Game date={selectedDate} />
+        <div className="not-motion-reduce:animate-slidefromtop relative h-screen select-none">
+            <div className="fill-gold bg-background absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <Chariot />
+            </div>
+            <div className="absolute top-1/2 left-1/2 w-30 -translate-x-1/2 -translate-y-1/2">
+                <div className="flex flex-col gap-2">
+                    <div className="text-foreground text-md m-2 rounded-lg p-2 text-center backdrop-blur-xl">
+                        {dateOnlyString(selectedDate)}
+                    </div>
+                    <Button
+                        isDisabled={false}
+                        handleButtonPress={() => setIsPlaying(true)}
+                    >
+                        {'Play'}
+                    </Button>
+                </div>
+            </div>
         </div>
     )
 }
