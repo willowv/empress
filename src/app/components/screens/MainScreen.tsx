dd4dfac7af490c270cc8624d02f2b5ed5adb348d
willowv <eps.willow@gmail.com>
2025-08-28 15:39:40 -0700

    Factor out more screens

---
+++ src/app/components/screens/MainScreen.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import Empress from '@/svg/tarot/Empress'
+import Button from '@/ui/Button'
+import SwipeNavigation from '@/ui/SwipeNavigation'
+import { useRouter } from 'next/navigation'
+
+export default function MainScreen() {
+    const router = useRouter()
+    return (
+        <div className="not-motion-reduce:animate-slidefromtop relative h-screen select-none">
+            <div className="fill-gold bg-background absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <SwipeNavigation>
+                    <Empress />
+                </SwipeNavigation>
+            </div>
+            <div className="absolute top-2/5 left-1/2 h-10 w-65 -translate-x-1/2 -translate-y-1/2 rounded-xl backdrop-blur-md" />
+            <div className="text-foreground font-empress absolute top-2/5 left-1/2 w-65 -translate-x-1/2 -translate-y-1/2 text-5xl">
+                {'The Empress Returns'}
+            </div>
+            <div className="bg-background absolute bottom-5 left-1/2 -translate-x-1/2 rounded-xl">
+                <Button handleButtonPress={() => router.push('/story')}>
+                    <div className="m-1 text-center text-xs">Story</div>
+                </Button>
+            </div>
+        </div>
+    )
+}
--- src/app/story/page.tsx
+++ src/app/story/page.tsx
@@ -1,9 +1,4 @@
-'use client'
-
 import StoryScreen from '@/screens/StoryScreen'
-import { useRouter } from 'next/navigation'
-
 export default function Home() {
-    const router = useRouter()
-    return <StoryScreen handleBackButton={() => router.push('/')} />
+    return <StoryScreen />
 }
