06b632f9303faad382d7f6eda2569f4e2d3473bc
willowv <eps.willow@gmail.com>
2025-09-03 16:29:13 -0700

    Extract DicePreview component

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -6,11 +6,10 @@ import {
 } from 'lib/util'
 import SwipeNavigation from '@/ui/SwipeNavigation'
 import ButtonLink from '@/ui/ButtonLink'
-import { getDiceCounts } from '@/logic/empress'
 import Scores from 'app/play/Scores'
 import QueryParamDateSelector from '@/ui/QueryParamDateSelector'
-import AgentPreview from '@/game/AgentPreview'
 import Fortune from '@/svg/tarot/Fortune'
+import DicePreview from './DicePreview'
 
 export default async function Page(props: {
     searchParams?: Promise<{
@@ -21,25 +20,6 @@ export default async function Page(props: {
     const dateString = searchParams?.date
     const selectedDate = ensureValidDate(dateString, getTodayWithoutTime())
     const oneYearAgo = addYears(getTodayWithoutTime(), -1)
-    const [mpDieSize_Count] = getDiceCounts({
-        date: selectedDate,
-        seed: dateOnlyString(selectedDate),
-        turnHistory: []
-    })
-    const previewDice = mpDieSize_Count
-        .entries()
-        .map(([dieSize, count], index) => {
-            if (count == 0) return
-            return (
-                <div
-                    key={`preview-${index}`}
-                    className="flex flex-row items-center gap-1"
-                >
-                    <div className="text-foreground text-md">{`${count} x`}</div>
-                    <AgentPreview dieSize={dieSize} />
-                </div>
-            )
-        })
     return (
         <div
             id="play-screen"
@@ -56,17 +36,7 @@ export default async function Page(props: {
                         max={getTodayWithoutTime()}
                         min={oneYearAgo}
                     />
-                    <div
-                        id="dice-preview"
-                        className="flex flex-col items-center gap-2 rounded-lg p-2 backdrop-blur-xl"
-                    >
-                        <div className="text-foreground text-md text-center">
-                            {'Dice Preview'}
-                        </div>
-                        <div className="flex min-w-64 flex-row flex-wrap items-center justify-center gap-2">
-                            {[...previewDice]}
-                        </div>
-                    </div>
+                    <DicePreview date={selectedDate} />
                     <div id="scores">
                         <Scores date={selectedDate} />
                     </div>
